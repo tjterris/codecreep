ac174ea16bd50ce217fe9ab727b5f309baaddfae
Trevor Terris <tjterris@gmail.com>
2015-05-27 22:45:28 -0400

    incomplete codecreep files

--- lib/codecreep.rb
+++ lib/codecreep.rb
@@ -5,6 +5,7 @@ require 'pry'
 require 'codecreep/init_db'
 require 'codecreep/github'
 require "codecreep/version"
+require "codecreep/user"
 
 
 module Codecreep
@@ -12,8 +13,33 @@ module Codecreep
     def initialize
       @github = Github.new
     end
+
+    def menu
+      puts "Would you like to fetch or analyze a user from Github?"
+      puts "Type 'fetch' or 'analyze'."
+      input = gets.chomp
+      if input == "fetch"
+        fetch
+      if input == "analyze"
+        analyze
+      end
+      end
+    end
+
+    def fetch
+      user = gets.chomp
+      @github.find_or_create_by(name: user) #Not recognizing Codecreep::User.new .. saying unitialized
+      
+    end
+
+    def analyze
+      #sorts data from database
+    end
+  
+
   end
 end
 
-# app = Codecreep::App.new
-# binding.pry
+app = Codecreep::App.new
+app.menu
+binding.pry
--- lib/codecreep/github.rb
+++ lib/codecreep/github.rb
@@ -5,5 +5,23 @@ module Codecreep
     include HTTParty
     base_uri 'https://api.github.com'
     basic_auth ENV['GH_USER'], ENV['GH_PASS']
+
+
+    def get_user(user)
+      self.class.get("/users/#{user}")
+    end
+    
+    def get_user_following(user)
+      self.class.get("/users/#{user}/following")
+    end
+
+    def get_user_followers(user)
+      self.class.get("/users/#{user}/followers")
+    end
+
+
   end
 end
+
+# 
+# 
